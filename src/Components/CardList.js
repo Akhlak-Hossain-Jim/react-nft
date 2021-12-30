c9faa864676c8f0030ddce0e8b6fe2bb357a9790
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2021-12-30 23:54:12 +0600

    bug-fixed

--- src/Components/CardList.js
+++ src/Components/CardList.js
@@ -2,12 +2,12 @@ import React from "react";
 import styled from "styled-components";
 import CardComponent from "./CardComponent";
 
-function CardList({ OSdata, setselectedData }) {
+function CardList({ OSdata, setSelectedData }) {
   return (
     <Container>
       {React.Children.toArray(
         OSdata.map((data) => (
-          <div onClick={() => setselectedData(data.token_id)}>
+          <div onClick={() => setSelectedData(data.token_id)}>
             <CardComponent
               name={data.name}
               id={data.token_id}
@@ -15,16 +15,6 @@ function CardList({ OSdata, setselectedData }) {
               img={data.image_preview_url}
             />
           </div>
-        ))
-      )}
-      {React.Children.toArray(
-        OSdata.map((data) => (
-          <CardComponent
-            name={data.name}
-            id={data.token_id}
-            traits={data.traits}
-            img={data.image_preview_url}
-          />
         ))
       )}
     </Container>
