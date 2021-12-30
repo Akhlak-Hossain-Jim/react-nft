950e1b5103202b3e9a054c0963b2c20e26121a94
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2021-12-30 23:53:34 +0600

    bug-fixed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
 import "./styles/App.css";
+import RawData from "./api/data.json";
 
 import Header from "./Layout/Header";
 import CardList from "./Components/CardList";
 import CardDetails from "./Components/CardDetails";
 
 function App() {
-  const [dataList, setdataList] = useState([]);
-  const [selectedCard, setselectedCard] = useState(0);
+  const [dataList, setDataList] = useState([]);
+  const [selectedCard, setSelectedCard] = useState(0);
 
   useEffect(() => {
-    const getData = async () => {
-      const openSeaData = await axios.get(
-        "https://testnets-api.opensea.io/assets?asset_contract_address=0xc8d401f1A3cE0DB2C17259D5C93AB6A23D9d96dB&order_direction=asc"
-      );
-      console.log(openSeaData.data.assets);
-      setdataList(openSeaData.data.assets);
-    };
-    return getData();
+    fetch(
+      "https://testnets-api.opensea.io/assets?asset_contract_address=0xc8d401f1A3cE0DB2C17259D5C93AB6A23D9d96dB&order_direction=asc"
+    )
+      .then((res) => res.json())
+      .then((res) => setDataList(res.assets))
+      .catch((error) => setDataList(RawData.assets));
   }, []);
+  console.log(dataList);
 
   return (
     <div className="app_container">
@@ -28,7 +27,7 @@ function App() {
         {dataList.length > 0 && (
           <>
             <CardDetails listData={dataList} selectedData={selectedCard} />
-            <CardList OSdata={dataList} setselectedData={setselectedCard} />
+            <CardList OSdata={dataList} setSelectedData={setSelectedCard} />
           </>
         )}
       </div>
