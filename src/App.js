d7e1b370dcd65db2ba320674c34306558f152594
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2021-12-30 23:23:39 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./styles/App.css";
+
+import Header from "./Layout/Header";
+import CardList from "./Components/CardList";
+import CardDetails from "./Components/CardDetails";
 
 function App() {
+  const [dataList, setdataList] = useState([]);
+  const [selectedCard, setselectedCard] = useState(0);
+
+  useEffect(() => {
+    const getData = async () => {
+      const openSeaData = await axios.get(
+        "https://testnets-api.opensea.io/assets?asset_contract_address=0xc8d401f1A3cE0DB2C17259D5C93AB6A23D9d96dB&order_direction=asc"
+      );
+      console.log(openSeaData.data.assets);
+      setdataList(openSeaData.data.assets);
+    };
+    return getData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app_container">
+      <div className="app">
+        <Header />
+        {dataList.length > 0 && (
+          <>
+            <CardDetails listData={dataList} selectedData={selectedCard} />
+            <CardList OSdata={dataList} setselectedData={setselectedCard} />
+          </>
+        )}
+      </div>
     </div>
   );
 }
